04c02f51407622fcab720e25ea07d67f4c3a0ed5
Najmus Sakib <nazmusakib412@gmail.com>
2022-05-12 01:59:31 +0600

    [UPDATED] footer style

--- src/components/Footer/Footer.comp.js
+++ src/components/Footer/Footer.comp.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className='text-center'>Fruit King ©2022 Created by Najmus Sakib</footer>
+        <footer className='text-center py-4 bg-dark text-white'>Fruit King ©2022 Created by Najmus Sakib</footer>
     )
 }
 
