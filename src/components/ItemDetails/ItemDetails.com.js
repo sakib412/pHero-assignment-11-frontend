d72d2e7c053321b9aea54af3f169d6f90b5a7e36
Najmus Sakib <nazmusakib412@gmail.com>
2022-05-13 20:03:33 +0600

    [ADDED] Item Details page is now fully working

--- src/components/ItemDetails/ItemDetails.com.js
+++ src/components/ItemDetails/ItemDetails.com.js
@@ -1,28 +1,61 @@
 import { Button, Form, InputNumber } from 'antd'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axiosInstance from '../../utils/axios'
 
 const ItemDetails = () => {
+    const { id } = useParams()
+    const [item, setItem] = useState({})
+    const [buttonDisabled, setButtonDisabled] = useState(false)
+    useEffect(() => {
+        axiosInstance.get(`/inventory/${id}`).then(({ data }) => {
+            setItem(data.results)
+        })
+    }, [id])
+
+    const onStockUpdate = ({ quantity }) => {
+        setButtonDisabled(true)
+        axiosInstance.put(`/inventory/${id}`, {
+            quantity
+        }).then(({ data }) => {
+            setItem(data.results)
+
+        }).finally(() => {
+            setButtonDisabled(false)
+        })
+        console.log(quantity)
+    }
 
     return (
         <div className='row d-flex justify-content-center align-items-center my-5'>
-            <h3 className='text-center'>Details</h3>
+            <h3 className='text-center'>ID: {item._id}</h3>
             <div className="col-md-4">
                 <img className='img-fluid' src='/images/cover.jpg' alt='' />
             </div>
             <div className="col-md-8">
-                <h3 className='text-center'>Item Name</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eligendi qui natus dolore iste, quibusdam sequi inventore architecto? Iure fugiat nesciunt molestias at optio quae quas itaque. Dolorem, exercitationem quidem.</p>
-                <p>Price: <strong>75 BDT</strong></p>
-                <p>Quantity: <strong>12</strong></p>
-                <p>Supplier: <strong>Najmus Sakib</strong> </p>
-                <button className='btn btn-primary mb-3'>Delevered</button>
+                <h3 className='text-primary'>{item?.name}</h3>
+                <p>{item?.description}</p>
+                <p>Price: <strong>{item.price} BDT</strong></p>
+                <p>Quantity: <strong>{item.quantity ? item.quantity : "Soldout"}</strong></p>
+                <p>Supplier: <strong>{item.supplier}</strong> </p>
+                <Button disabled={buttonDisabled || !item.quantity} className='mb-3' type='primary' onClick={() => {
+                    onStockUpdate({ quantity: -1 })
+                }}>Delevered</Button>
 
-                <h6 className='mt-3 text-primary'>Update quantity</h6>
-                <Form layout='inline'>
-                    <Form.Item name="quantity">
+                <h6 className='mt-3 text-primary'>Increase quantity</h6>
+                <Form
+                    onFinish={onStockUpdate}
+                    layout='inline'
+                    initialValues={{ "quantity": 1 }}>
+                    <Form.Item
+                        name="quantity"
+                        rules={[
+                            { type: "number", min: 1 },
+                            { type: "number", max: 100 }
+                        ]}>
                         <InputNumber />
                     </Form.Item>
-                    <Button htmlType='submit' type='primary'>Add</Button>
+                    <Button htmlType='submit' disabled={buttonDisabled} type='primary'>Add</Button>
                 </Form>
 
             </div>
