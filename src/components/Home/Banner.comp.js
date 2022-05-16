1b22c076722d3d2dae3da141afe6d3d2922abc83
Najmus Sakib <nazmusakib412@gmail.com>
2022-05-17 03:34:45 +0600

    [UPDATED] Banner Heading and description

--- src/components/Home/Banner.comp.js
+++ src/components/Home/Banner.comp.js
@@ -5,8 +5,8 @@ const Banner = () => {
     return (
         <section className="banner position-relative py-3 py-md-5 text-center">
             <div className="col-md-5 py-lg-5 mx-auto my-5">
-                <h1 className="display-4 font-weight-normal text-white">Punny headline</h1>
-                <p className="lead font-weight-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple's marketing pages.</p>
+                <h1 className="display-4 font-weight-normal text-white">Fruit King</h1>
+                <p className="lead font-weight-normal">Directly from the farm, freshly grown for our clients.</p>
             </div>
             <div className="product-device box-shadow d-none d-md-block"></div>
             <div className="product-device product-device-2 box-shadow d-none d-md-block"></div>
